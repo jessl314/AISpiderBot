f9f46b75fdb213987bf551caecc8428e93e6913a
jellollama <chiucady107@gmail.com>
2026-02-06 02:42:08 +0000

    2 layer neural network set uo

--- neural_network.py
+++ neural_network.py
@@ -15,3 +15,41 @@ def nonlin(x,deriv=False):
     if(deriv==True):
         return x*(1-x)
     return 1/(1+np.exp(-x))
+
+# input dataset
+X = np.array([[0,0,1],
+              [0,1,1],
+              [1,0,1],
+              [1,1,1]])
+
+# output dataset
+y = np.array([[0,0,1,1]]).T
+
+# input/output dataset matrix where each row is a training example
+
+# seed random num to make calculation
+# deterministic (good practice)
+np.random.seed(1)
+
+# initialize weights randomly with mean 0
+syn0 = 2*np.random.random((3,1)) - 1
+
+for iter in range(10000):
+    # forward propagation
+    l0 = X
+    # l0 is first layer of network, specified by input data
+    l1 = nonlin(np.dot(l0,syn0))
+    # l1 is second layer, aka hidden layer
+
+    # how much missed
+    l1_error = y - l1
+
+    # multiply how much missed by
+    # slope (derivative) of sigmoid at values in l1
+    l1_delta = l1_error * nonlin(l1,True)
+    # This line does the most
+
+    # update weights
+    syn0 += np.dot(l0.T,l1_delta)
+
+print(f"Output after training:\n{l1}")
