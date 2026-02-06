5f52974f6f73bf88c241bc2d7c21aef3ec5e47b9
jellollama <chiucady107@gmail.com>
2026-02-06 02:27:58 +0000

    nonlinearity function that maps sigmoid function so it's an value btwn 0 and 1, then converts it to probability

--- neural_network.py
+++ neural_network.py
@@ -5,3 +5,13 @@
 # Backpropagation - measures statistics to make a model
 
 import numpy as np
+# numpy is linear algebra library
+
+# sigmoid function - S-shaped/sigmoid curve
+# sigmoid func maps number into probability value btwn 1 and 0
+# output of sigmoid func can be used to create it's derivative (slope)
+def nonlin(x,deriv=False):
+# nonlinearity that maps sigmoid function
+    if(deriv==True):
+        return x*(1-x)
+    return 1/(1+np.exp(-x))
