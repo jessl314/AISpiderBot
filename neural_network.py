5d5ca8113b2f17ffdfee1f6e0d7b6965d73a5c05
jellollama <chiucady107@gmail.com>
2026-02-06 02:17:56 +0000

    learning neural network basics

---
+++ neural_network.py
@@ -0,0 +1,7 @@
+# following "A Neural Network in 11 lines of Python (Part 1) article" by iamtrask
+
+# Part 1: Tiny Toy Network
+# neural network trained w/ backprop attempting to use input to predict output
+# Backpropagation - measures statistics to make a model
+
+import numpy as np
